31b0f823742769f8b21099aa4ea53208394b6c58
liuss7 <liuss7@asiainfo.com>
2022-09-06 21:24:33 +0800

    提取路由

--- vue-admin/src/router/autoload.ts
+++ vue-admin/src/router/autoload.ts
@@ -1,16 +1,38 @@
 import { RouteRecordRaw } from "vue-router";
 
 const layouts = import.meta.glob("../layouts/*.vue");
-console.log("layouts", layouts);
-Object.entries(layouts).forEach(([file, module]) => {
-  const route = getRouteByModule(file, module)
-})
+const view =import.meta.glob("../view/**/*.vue");
+
+
+function getRoutes(){
+  const layoutRoutes = [] as RouteRecordRaw[];
+  Object.entries(layouts).forEach(([file, module]) => {
+    const route = getRouteByModule(file, module);
+    route.children=getChildrenRoutes(route)
+    layoutRoutes.push(route)
+  })
+  console.log("asdfasdf",layoutRoutes);
+  
+  return layoutRoutes
+}
+
+function getChildrenRoutes(layoutRoute:RouteRecordRaw){
+  const routers=[] as RouteRecordRaw[]
+     Object.entries(view).forEach(([file,module])=>{
+      if(file.includes(`../view/${layoutRoute.name as string}`)){
+        const route=getRouteByModule(file,module)
+        routers.push(route)
+      }
+     })
+
+     return routers
+}
 
 function getRouteByModule(file: string, module: { [key: string]: any }) {
   // console.log(file.match(/\.\.\/layouts\/(?<name>.+?)\.vue/i)?.groups?.name);
-  const name = file.replace(/.+layouts\/|\.vue/gi, "")
+  const name = file.replace(/.+layouts\/|.+view\/|\.vue/gi, "")
   const route = {
-    name,
+    name:name.replace("/","."),
     path: `/${name}`,
     component: module
 
@@ -20,7 +42,8 @@ function getRouteByModule(file: string, module: { [key: string]: any }) {
 }
 
 
-const layoutRoutes = [] as RouteRecordRaw[];
+
+console.log(getRoutes());
 
 
-export default layoutRoutes
+export default getRoutes()
