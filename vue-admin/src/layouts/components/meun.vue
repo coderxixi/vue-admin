e20eb225aa157626bfefbf6b33a886184cc1f3e2
liuss7 <976344695@qq.com>
2022-10-11 20:44:46 +0800

    顶部导航样式处理

--- vue-admin/src/layouts/components/meun.vue
+++ vue-admin/src/layouts/components/meun.vue
@@ -11,7 +11,7 @@
         </div>
         <i class="fas fa-angle-down"></i>
       </dt>
-      <dd v-show="item.active" @click="clickDd(val)" v-for="val of item.children" :key="val.name">
+      <dd v-show="item.active" @click="clickDd(val)" v-for="val of item.children" :key="val.title">
         <div
           :class="{ active: val.active }"
           class="bg-zinc-600 mt-2 py-2 rounded-md text-white text-center"
--- vue-admin/src/layouts/components/navBar.vue
+++ vue-admin/src/layouts/components/navBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-between items-center pr-4 pl-4 h-[48px] bg-white">
-    <div class="flex items-center">
+    <!-- <div class="flex items-center">
       <MenuUnfoldOne theme="filled" size="24" fill="#10ad57" class="cursor-pointer duration-300" v-if="show" />
       <MenuFoldOne theme="filled" size="24" fill="#10ad57" class="cursor-pointer duration-300" v-else />
       <div class="ml-8 "><span class=" from-stone-800 text-[18px]">首页</span> / <span class=" from-stone-800 text-[18px]">错误页面</span> / 404</div>
@@ -9,16 +9,58 @@
       <i class="fas cursor-pointer fa-expand hidden md:flex mr-3 text-gray-600" />
       <img class="w-[30px] rounded-3xl" src="../../assets/img/avar.jpeg"/>
       <span class=" ml-3">嘻嘻</span>
+    </div> -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">编辑器</el-breadcrumb-item>
+      <el-breadcrumb-item>富文本编辑器</el-breadcrumb-item>
+      <!-- <el-breadcrumb-item>promotion detail</el-breadcrumb-item> -->
+    </el-breadcrumb>
+
+    <div class="flex items-center justify-center relative group">
+      <i
+        class="fas cursor-pointer fa-expand hidden md:flex mr-3 text-gray-600"
+      />
+      <img class="w-[30px] rounded-3xl" src="../../assets/img/avar.jpeg" />
+      <span class="ml-3 text-sm text-gray-600">嘻嘻</span>
+      <!-- 弹出菜单 -->
+      <section
+        class="
+          border
+          rounded-md
+          absolute
+         top-full
+          bg-white
+          shadow-sm
+          px-3
+          whitespace-nowrap
+          hidden
+          group-hover:block
+        "
+      >
+        <div class="flex  items-center border-b py-3 cursor-pointer">
+          <i class="fab fa-blogger"></i>
+          <span class="text-sm text-gray-600 ml-2">网站首页</span>
+        </div>
+        <div class="flex  items-center  border-b  py-2  cursor-pointer">
+          <i class="fab fa-blogger"></i>
+          <span class="text-sm text-gray-600 ml-2">会员中心</span>
+        </div>
+        <div class="flex  items-center  py-2  cursor-pointer">
+          <i class="fab fa-blogger"></i>
+          <span class="text-sm text-gray-600 ml-2">用户管理</span>
+        </div>
+      
+      </section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { MenuFoldOne, MenuUnfoldOne } from '@icon-park/vue-next'
-import { ref } from 'vue';
-let show=ref(true)
+import { MenuFoldOne, MenuUnfoldOne } from "@icon-park/vue-next";
+import { ref } from "vue";
+let show = ref(true);
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
