5e5b295726b7b7b1a665b37572a8defd04964dfe
liuss7 <976344695@qq.com>
2022-10-11 21:26:39 +0800

    动态设置面板数据

--- vue-admin/src/layouts/components/historyLink.vue
+++ vue-admin/src/layouts/components/historyLink.vue
@@ -1,38 +1,42 @@
 
 
 <template>
-  <div  class="m-3 grid grid-flow-col gap-3 justify-start">
-     <a  class=" text-sm text-gray-600 bg-white rounded-md py-2 px-3" href="#" v-for="(n,index) of links" :key="index">
-     {{n.title}}
+  <div class="m-3 grid grid-flow-col gap-3 justify-start">
+    <a
+      class="text-sm text-gray-600 bg-white rounded-md py-2 px-3"
+      href="#"
+      v-for="(n, index) of links"
+      :key="index"
+    >
+      {{ n.title }}
       <i class="fas fa-times ml-1"></i>
-     </a>
+    </a>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-const links=ref<{title:string}[]>([
+const links = ref<{ title: string }[]>([
   {
-    title:'404错误'
+    title: "404错误",
   },
   {
-    title:'富文本编辑器'
+    title: "富文本编辑器",
   },
   {
-    title:'用户管理'
+    title: "用户管理",
   },
   {
-    title:'404错误'
+    title: "404错误",
   },
   {
-    title:'富文本编辑器'
+    title: "富文本编辑器",
   },
   {
-    title:'用户管理'
+    title: "用户管理",
   },
 ]);
 </script>
 
 <style scoped>
-
 </style>
--- vue-admin/src/view/admin/home.vue
+++ vue-admin/src/view/admin/home.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="grid grid-flow-col gap-3">
-    <el-card shadow="hover" :body-style="{ padding: '20px' }" v-for="n of 4">
+    <el-card shadow="hover" :body-style="{ padding: '20px' }" v-for="(n,index) of data" :key="index">
       <template #header>
         <div class="flex justify-between items-center">
-          用户数
+         {{n.title}}
           <el-tag type="danger" size="mini" effect="dark">月</el-tag>
         </div>
       </template>
       <section class="flex justify-between items-center">
-        <span class="text-2xl"> $2998989</span>
+        <span class="text-2xl"> {{n.price}}</span>
         <i class="fab fa-algolia text-violet-500 text-4xl"></i>
       </section>
       <section class="text-xs flex justify-between mt-5">
-        总人数 <span> 200人</span>
+        {{n.totalTitle}}<span> {{n.total}}</span>
       </section>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+ interface ICard {
+  title:string,
+  price:number,
+  icon:string,
+  totalTitle:string,
+  total:number
+
+ }
+  const data=ref<ICard[]>([
+    {title:'总人数',icon:'fab fa-algolia',price:233343,total:87685678,totalTitle:'总人数'},
+    {title:'销售额',icon:'fab fa-accessible-icon',price:233343,total:87685678,totalTitle:'总销售额'},
+    {title:'订单数',icon:'fab fa-angellist',price:233343,total:87685678,totalTitle:'总订单数'},
+    {title:'评论数',icon:'fab fa-aviato',price:233343,total:87685678,totalTitle:'总评论数'}
+
+    ])
 </script>
 <script lang="ts">
 export default {
